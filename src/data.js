f32364945913387bb4eb0bfb4bd2619d84b3bbf1
Christopher Luvumbo <christopherluvumbo.dev@gmail.com>
2022-05-31 17:25:46 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'random',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: '/projects',
+    text: 'projects',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'contact',
+  },
+  {
+    id: 5,
+    url: '/profile',
+    text: 'profile',
+  },
+  {
+    id: 6,
+    url: '/example1',
+    text: 'example1',
+  },
+  {
+    id: 7,
+    url: '/example2',
+    text: 'example2',
+  },
+]
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    icon: <FaBehance />,
+  },
+]
